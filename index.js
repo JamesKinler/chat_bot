50047e79c383e475a75156dba83d0b77e16e5833
JamesKinler <james193722@yahoo.com>
2021-01-20 10:32:34 -0800

    updating files

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const app = express()
 app.use(bodyParser.json())
 const port = process.env.PORT || 3000
 
+const TYPE_OF_Quote = 'TypeOfQuote'
+
+
+// Weather Code
+// const request = require('request');
+// const apiKey = 'f50383b08ce3928555c6f2b6a6e21d3a';
+//
+// const city = 'Fresno';
+// const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+//
+// request(url, (error, response, body) => {
+//   console.log(body);
+// })
+
 
 app.post('/chat-bot', (request, response) => {
   chatBot(request, response)
@@ -21,7 +35,13 @@ const chatBot = (request, response) => {
   function sayHello(agent){
     agent.add('Hello my name is Austin how can i help you?')
   }
+
+  function giveQuote(agent){
+    agent.add("No Matter what people tell you, words and ideas can change the world")
+  }
+
   let intentMap = new Map();
   intentMap.set("Default Welcome Intent", sayHello)
+  intentMap.set(TYPE_OF_Quote, giveQuote)
   agent.handleRequest(intentMap)
 }
