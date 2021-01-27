0f3e2499dd37e56cd797c65f40975ed622aadf64
JamesKinler <james193722@yahoo.com>
2021-01-27 12:00:43 -0800

    adding weather to the chatbot

--- index.js
+++ index.js
@@ -48,8 +48,9 @@
   }
 
   function weatherMap(agent){
-    const weather_city = agent.parameters['geo-city-us'].toLowerCase();
+    const weather_city = agent.parameters['sys.geo-city-us'].toLowerCase();
     console.log(weather_city);
+    console.log('test');
     // agent.add(`Its rainy in ${weather_city}`)
   }
 
