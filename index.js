a8e962485559e6fe8df00d8975a95c09dda438e8
JamesKinler <james193722@yahoo.com>
2021-01-27 13:41:05 -0800

    adding weather to the chatbot

--- index.js
+++ index.js
@@ -48,7 +48,7 @@
   }
 
   function weatherMap(agent){
-    // const weather_city = agent.parameters['geo-city-us'].toLowerCase();
+    const weather_city = agent.parameters['geo-city-us'].toLowerCase();
     const request = require('request');
     const apiKey = 'f50383b08ce3928555c6f2b6a6e21d3a';
 
