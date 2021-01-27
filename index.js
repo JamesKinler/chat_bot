ac011e16cd0b729aa6e02c33ae9735472bd28a75
JamesKinler <james193722@yahoo.com>
2021-01-27 13:50:34 -0800

    adding weather to the chatbot

--- index.js
+++ index.js
@@ -55,10 +55,10 @@
     const city = 'Fresno';
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-    request(url, (error, response, body), agent => {
+    request(url, (error, response, body) => {
       const data = JSON.parse(body);
       console.log(`It's currently ${data.main.temp}`);
-      agent.add(`Its rainy in ${weather_city}`)
+      // agent.add(`Its rainy in ${weather_city}`)
     })
 
     // console.log(weather_city);
