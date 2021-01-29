0cccc62d3e9caa33fcd990dba0ccb825053f574e
JamesKinler <james193722@yahoo.com>
2021-01-29 12:35:35 -0800

    make it better

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const {WebhookClient} = require('dialogflow-fulfillment');
+  const express = require('express')
+  const bodyParser = require('body-parser')
+  const {WebhookClient} = require('dialogflow-fulfillment');
 
-const app = express()
-app.use(bodyParser.json())
-const port = process.env.PORT || 3000
+  const app = express()
+  app.use(bodyParser.json())
+  const port = process.env.PORT || 3000
 
-app.post('/chat-bot', (request, response) => {
-    dialogflowFulfillment(request, response)
-})
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+  app.post('/chat-bot', (request, response) => {
+    chatBot(request, response)
 
-const dialogflowFulfillment = (request, response) => {
-    const agent = new WebhookClient({request, response})
+  })
 
-    function sayHello(agent){
-        agent.add("Hello, this was a nice tutorial by axlewebtech")
+
+  app.listen(port, () => {
+    console.log(`listing on port ${port}`)
+  })
+
+  const chatBot = (request, response) => {
+  const agent = new WebhookClient({request, response})
+
+  function sayHello(agent){
+    agent.add("Hello my name is Austin how can i help you?")
+  }
+
+  function giveQuote(agent){
+    const quote_type = agent.parameters['TypeOfQuote'].toLowerCase();
+    console.log(quote_type);
+    if(quote_type == "inspiration"){
+      agent.add("The world is filled with roses. You need to pick one and smell it!")
+    }else{
+        agent.add("No Matter what people tell you, words and ideas can change the world")
     }
+  }
 
-    let intentMap = new Map();
-    intentMap.set("Default Welcome Intent", sayHello)
-    agent.handleRequest(intentMap)
 
+  let intentMap = new Map();
+  intentMap.set("Default Welcome Intent", sayHello)
+  intentMap.set("Need Quote", giveQuote)
+  agent.handleRequest(intentMap)
 }
