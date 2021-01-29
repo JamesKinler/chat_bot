7be8eb3b070e4938a917bab84a3739dcf3d552ce
JamesKinler <james193722@yahoo.com>
2021-01-29 12:09:41 -0800

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
   const express = require('express')
   const bodyParser = require('body-parser')
   const {WebhookClient} = require('dialogflow-fulfillment');
-  const http = require('http')
+  const axios = require('axios');
 
   const app = express()
   app.use(bodyParser.json())
@@ -10,8 +10,21 @@
 
   // Weather Code
   // const request = require('request');
-
+  const apiKey = 'f50383b08ce3928555c6f2b6a6e21d3a';
   //
+  const city = 'Fresno';
+  // const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+
+  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
   // request(url, (error, response, body) => {
   //   const data = JSON.parse(body);
   //   console.log(`It's currently ${data.main.temp}`);
@@ -55,29 +68,6 @@
   function weatherMap(agent){
 
     const weather_city = agent.parameters['geo-city-us'].toLowerCase();
-
-    const apiKey = 'f50383b08ce3928555c6f2b6a6e21d3a';
-    //
-    // const city = 'Fresno';
-    // const url = ``;
-
-    http.get(`http://api.openweathermap.org/data/2.5/weather?q=${weather_city}&units=imperial&appid=${apiKey}`, (resp) => {
-    let data = '';
-
-    // A chunk of data has been received.
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    resp.on('end', () => {
-      const weatherData = JSON.parse(data);
-     console.log(`It's currently ${weatherData.main.temp}`);
-     agent.add(`It's currently ${weatherData.main.temp}`)
-   });
-
-  }).on("error", (err) => {
-   console.log("Error: " + err.message);
-  });
 
 
     // console.log(weather_city);
