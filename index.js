d260b8936a3908790b8d8dcdff2f95c61e081446
JamesKinler <james193722@yahoo.com>
2021-08-16 18:05:34 -0700

    updating the image function

--- index.js
+++ index.js
@@ -39,7 +39,7 @@
   function giveImage(agent){
     agent.add(new Image({
       imageUrl: 'https://cdn1-pornstars.4tube.com/tb/0/0/0/0/0/7/5/1/0/1512479574_275x375.jpg',
-      display: 'DEFAULT'
+      // display: 'DEFAULT'
     })
   )
     // const demo_jpg = "https://cdn1-pornstars.4tube.com/tb/0/0/0/0/0/7/5/1/0/1512479574_275x375.jpg";
