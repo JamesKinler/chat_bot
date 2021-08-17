602bf98bbd1c2a097846779668f50b67fbdd80ae
JamesKinler <james193722@yahoo.com>
2021-08-16 17:19:04 -0700

    updating the image function

--- index.js
+++ index.js
@@ -31,7 +31,7 @@
     const demo_jpg = "https://cdn1-pornstars.4tube.com/tb/0/0/0/0/0/7/5/1/0/1512479574_275x375.jpg";
     console.log('sent');
     agent.add('Here is your image');
-    agent.add(new Image('https://cdn1-pornstars.4tube.com/tb/0/0/0/0/0/7/5/1/0/1512479574_275x375.jpg'));
+    agent.add(new Image('https://cdn1-pornstars.4tube.com/tb/0/0/0/0/0/7/5/1/0/1512479574_275x375.jpg'), display: 'DEFAULT');
   }
 
   function giveQuote(agent){
