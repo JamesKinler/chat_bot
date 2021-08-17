6731d9809aec6e046d8e77e04817bd467847b1f0
JamesKinler <james193722@yahoo.com>
2021-08-16 18:23:37 -0700

    updating the image function

--- index.js
+++ index.js
@@ -30,8 +30,7 @@
 
   function giveImage(agent){
     agent.add(new Image({
-      imageUrl: 'https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png',
-      display: 'DEFAULT'
+      imageUrl: 'https://images.unsplash.com/photo-1516125073169-9e3ecdee83e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&w=1000&q=80',
     })
   )
     // const demo_jpg = "https://cdn1-pornstars.4tube.com/tb/0/0/0/0/0/7/5/1/0/1512479574_275x375.jpg";
