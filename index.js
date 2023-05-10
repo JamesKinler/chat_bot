67382ae3c0ce9388054a564a2ac659bc78023e4e
JamesKinler <james193722@yahoo.com>
2023-05-09 23:04:43 -0700

    added weather to the chat bot

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
   const express = require('express')
+  require('dotenv').config()
   // const bodyParser = require('body-parser')
   const {WebhookClient} = require('dialogflow-fulfillment');
   // const {Card,Suggestion,Image,Payload} = require('dialogflow-fulfillment');
-  // const axios = require('axios');
-
+  const axios = require('axios');
   const app = express()
   app.use(express.json())
   const port = process.env.PORT || 3000
@@ -28,8 +28,29 @@
     agent.add("Hello my name is Austin how can i help you?")
   }
 
+  function weatherApi(agent){
+    console.log('test')
+    agent.add("its cold")
+    const weather_city = agent.parameters['geo-city'].toLowerCase();
+    console.log(weather_city);
+    return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${weather_city}&units=imperial&APPID=${process.env.WEATHER_APP_ID}`)
+  .then((response) => {
+      // handle success
+      // console.log(response.data);
+      const temp = response.data.main.temp;
+      agent.add(`the current temperature is ${temp}`);
+
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    })
+  }
+
 
   let intentMap = new Map();
   intentMap.set("Default Welcome Intent", sayHello)
+  intentMap.set("Weather", weatherApi)
   agent.handleRequest(intentMap)
+  
 }
